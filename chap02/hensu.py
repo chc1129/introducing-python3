a32317559ec25f66e3fb3137a5db64e6b2c9c6ec
chicu <chc1129@gmail.com>
2018-12-23 12:08:47 +0900

    num first exp

---
+++ chap02/hensu.py
@@ -0,0 +1,42 @@
+a = 95
+
+print(a)
+
+print(a - 3)
+
+print(a)
+
+a = a - 3
+print(a)
+
+a = 95
+temp = a - 3
+a = temp
+print(temp)
+
+a = a - 3
+print(a)
+
+a = 95
+a -= 3
+print(a)
+
+a += 8
+print(a)
+
+a *= 2
+print(a)
+
+a /= 3
+print(a)
+
+a = 13
+a //= 4
+print(a)
+
+print(9 % 5)
+
+print(divmod(9,5))
+
+print(9 // 5)
+print(9 % 5)
