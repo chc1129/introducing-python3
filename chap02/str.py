715c460e7908c3308b300d1c19060ec96da09045
chicu <chc1129@gmail.com>
2018-12-23 14:15:35 +0900

    str first exp

---
+++ chap02/str.py
@@ -0,0 +1,32 @@
+print('Snap')
+print('Crackle')
+print("'Nay,' said the nyasayer.")
+print('The rare double quote in captivity; ".')
+print('A two by four" is actually 1 1/2" x 3 1/2".')
+print("'There's the man that shot my paw!' cried the limping hound.")
+
+print('''Boom!''')
+print("""Eek!""")
+
+poem =  '''There was a Young Lady of Norway,
+Who casually sat in a doorway;
+When the door squeezed her flat,
+She exclaimed, "What o that?"
+This courageous Young Lady of Norway.'''
+print(poem)
+
+poem2 =  '''I do not like thee, Fell.
+    The reason why, I cannot tell.
+    But this I know, and know full well:
+    I do not like three, Doctor Fell.
+'''
+print(poem2)
+
+print(99, 'bottles', 'would be enough.')
+
+bottles = 99
+base = ''
+base += 'current inventory: ' # 在庫
+base += str(bottles)
+print(base)
+
