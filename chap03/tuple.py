01f236f93d9e2bd1258d94aeccfe8fa5e2ef73c5
chicu <chc1129@gmail.com>
2018-12-24 17:18:36 +0900

    tuple sample 1st commit

---
+++ chap03/tuple.py
@@ -0,0 +1,24 @@
+empty_tuple = ()
+print(empty_tuple)
+
+one_marx = 'Groucho',
+print(one_marx)
+
+marx_tuple = 'Groucho', 'Chico', 'Harpo'
+print(marx_tuple)
+
+marx_tuple = ('Groucho', 'Chico', 'Harpo')
+print(marx_tuple)
+a, b, c = marx_tuple
+print(a)
+print(b)
+print(c)
+
+password = 'swordfish'
+icecream = 'tuttifrutti'
+password, icecream = icecream, password
+print(password)
+print(icecream)
+
+marx_list = ['Groucho', 'Chico', 'Harpo']
+print(tuple(marx_list))
