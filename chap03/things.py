6215dc919be040000c42a3b1f909fa71bcb9ba11
chicu <chc1129@gmail.com>
2018-12-28 00:27:25 +0900

    3.8 3-7 commit

--- chap03/things.py
+++ chap03/things.py
@@ -1,2 +1,7 @@
-things = ['mozzarell', 'cinderella', 'salmonella']
+things = ['mozzarella', 'cinderella', 'salmonella']
+print(things)
+print(things[1].capitalize())
+print(things)
+print(things[0].upper())
+things.pop()
 print(things)
