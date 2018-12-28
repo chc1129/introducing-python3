a91c0cee77884f81d5dad8a5907a676e0850bb19
chicu <chc1129@gmail.com>
2018-12-28 15:48:12 +0900

    3.8 3-18 commit

---
+++ chap03/life.py
@@ -0,0 +1,13 @@
+life = {
+    'animals': {
+        'cats': ['Henri', 'Grumpy', 'Lucy'],
+        'octopi': {},
+        'emus': {}
+    },
+    'plants': {},
+    'other': {}
+}
+print(life)
+print(life.keys())
+print(life['animals'].keys())
+print(life['animals']['cats'])
