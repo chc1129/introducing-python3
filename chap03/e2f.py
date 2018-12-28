3ae34d019aaa559a74f8bd1c37ae3185472233a7
chicu <chc1129@gmail.com>
2018-12-28 14:22:19 +0900

    3.8 3-12 commit

---
+++ chap03/e2f.py
@@ -0,0 +1,6 @@
+e2f = {"dog": "chien", "cat": "chat", "walrus": "morse"}
+print(e2f)
+print(e2f["walrus"])
+f2e = {value: key for key, value in e2f.items()}
+print(f2e)
+print(f2e["chien"])
---
+++ chap03/surprise.py
@@ -0,0 +1,8 @@
+surprise = ['Groucho', 'Chico', 'Harpo']
+print(surprise)
+surprise[-1] = surprise[-1].lower()
+print(surprise)
+surprise.sort(reverse=True)
+print(surprise)
+surprise[0] = surprise[0].upper()
+print(surprise)
