2c4c0258bfcbd1c584f309bb2797ed8427bf4824
chicu <chc1129@gmail.com>
2019-01-04 13:27:32 +0900

    itertools, pprint sample

---
+++ chap05/itertools.py
@@ -0,0 +1,12 @@
+import itertools
+for item in itertools.chain([1, 2], ['a', 'b']):
+    print(item)
+
+for item in itertools.accumulate([1, 2, 3, 4]):
+    print(item)
+
+def multiply(a, b):
+    return a * b
+
+for item in itertools.accumulate([1, 2, 3, 4], multiply):
+    print(item)
