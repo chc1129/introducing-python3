34010f6c72f9974060ae4a446a723d9626c8ac80
chicu <chc1129@gmail.com>
2019-01-10 02:45:16 +0900

    special method sample 2nd

--- chap06/spmethod2.py
+++ chap06/spmethod2.py
@@ -3,6 +3,10 @@ class Word():
         self.text = text
     def __eq__(self, word2):
         return self.text.lower() == word2.text.lower()
+    def __str__(self):
+        return self.text
+    def __repr__(self):
+        return 'Word("' + self.text + '")'
 
 first = Word('ha')
 second = Word('HA')
