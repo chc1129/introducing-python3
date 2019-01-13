2a82b6e7468b3280b165cba6c64ebce50424a6ba
chicu <chc1129@gmail.com>
2019-01-13 23:26:43 +0900

    6.15 6-8

--- chap06/Element.py
+++ chap06/Element.py
@@ -1,22 +1,19 @@
 class Element:
     def __init__(self, name, symbol, number):
-        self.name = name
-        self.symbol = symbol
-        self.number = number
-    def __str__(self):
-        return('name=%s, symbol=%s, number=%s' % (self.name, self.symbol, self.number))
+        self.__name = name
+        self.__symbol = symbol
+        self.__number = number
+    @property
+    def name(self):
+        return self.__name
+    @property
+    def symbol(self):
+        return self.__symbol
+    @property
+    def number(self):
+        return self.__number
 
 hydrogen = Element('Hydrogen', 'H', 1)
-print(hydrogen)
 print(hydrogen.name)
 print(hydrogen.symbol)
 print(hydrogen.number)
-
-el_dict = {'name': 'Hydrogen', 'symbol': 'H', 'number': 1}
-hydrogen = Element(el_dict['name'], el_dict['symbol'], el_dict['number'])
-print(hydrogen.name)
-print(hydrogen.symbol)
-print(hydrogen.number)
-
-hydrogen = Element(**el_dict)
-print(hydrogen)
