105191248c80e3334e55d1371ffaa8efa380d87f
chicu <chc1129@gmail.com>
2019-01-13 21:44:54 +0900

    6.15 6-6

--- chap06/Element.py
+++ chap06/Element.py
@@ -6,5 +6,20 @@ class Element:
     def __str__(self):
         print('name=%s, symbol=%s, number=%s' % (self.name, self.symbol, self.number))
 
+hydrogen = Element('Hydrogen', 'H', 1)
+print(hydrogen)
+print(hydrogen.name)
+print(hydrogen.symbol)
+print(hydrogen.number)
+
+el_dict = {'name': 'Hydrogen', 'symbol': 'H', 'number': 1}
+hydrogen = Element(el_dict['name'], el_dict['symbol'], el_dict['number'])
+print(hydrogen.name)
+print(hydrogen.symbol)
+print(hydrogen.number)
+
+hydrogen = Element(**el_dict)
+print(hydrogen.name)
+
 hydrogen = Element(**el_dict)
 hydrogen.dump()
