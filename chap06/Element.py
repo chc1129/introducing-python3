1fe876fa3ca4e05a7a58fc93153ad2a0411a6400
chicu <chc1129@gmail.com>
2019-01-13 21:43:49 +0900

    6.15 6-6

--- chap06/Element.py
+++ chap06/Element.py
@@ -3,23 +3,8 @@ class Element:
         self.name = name
         self.symbol = symbol
         self.number = number
-    def dump(self):
+    def __str__(self):
         print('name=%s, symbol=%s, number=%s' % (self.name, self.symbol, self.number))
-
-hydrogen = Element('Hydrogen', 'H', 1)
-print(hydrogen)
-print(hydrogen.name)
-print(hydrogen.symbol)
-print(hydrogen.number)
-
-el_dict = {'name': 'Hydrogen', 'symbol': 'H', 'number': 1}
-hydrogen = Element(el_dict['name'], el_dict['symbol'], el_dict['number'])
-print(hydrogen.name)
-print(hydrogen.symbol)
-print(hydrogen.number)
-
-hydrogen = Element(**el_dict)
-print(hydrogen.name)
 
 hydrogen = Element(**el_dict)
 hydrogen.dump()
