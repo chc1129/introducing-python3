6192dd1f10bee9b5d20c3fa1165b47fc7b164ed1
chicu <chc1129@gmail.com>
2019-01-22 01:35:39 +0900

    chapter 07 sample

---
+++ chap07/percent.py
@@ -0,0 +1,25 @@
+print('%s' % 42)
+print('%s' % 42)
+print('%x' % 42)
+print('%o' % 42)
+print('%s' % 7.03)
+print('%f' % 7.03)
+print('%e' % 7.03)
+print('%g' % 7.03)
+print('%d%%' % 100)
+
+actor = 'Richard Gere'
+cat = 'Chester'
+weight = 26
+print("My wife's favorite actor is %s" % actor)
+print("Our cat %s wights %s pounds" $ (cat, wwight))
+
+n = 42
+f = 7.93
+s = 'string cheese'
+print('%d %f %s' % (n, f, s))
+print('%10d %10f %10s' % (n, f, s))
+print('%-10d %-10f %-10s' % (n, f, s))
+print('%10.4d %10.4f %10.4s' % (n, f, s))
+print('%.4d %.4f %.4s' % (n, f, s))
+print('%*.*d %*.*f %*.*s' % (10, 4, n, 10, 4, f, 10, 4, s)
