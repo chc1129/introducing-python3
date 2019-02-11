7e85076ca6a33a12849f84b398ef2cecd37b8055
chicu <chc1129@gmail.com>
2019-02-12 00:30:00 +0900

    7.1.3.8 patern

--- chap07/meta.py
+++ chap07/meta.py
@@ -14,3 +14,13 @@ print(re.findall('I (?=wish)', source))
 print(re.findall('(?<=I) wish', source))
 print(re.findall('\bfish', source))
 print(re.findall(r'\bfish', source))
+
+m = re.search(r'(. dish\b).*(\bfish)', source)
+print(m.group())
+print(m.groups())
+
+m = re.search(r'(?P<DISH>. dish\b).*(?P<FISH>\bfish)', source)
+print(m.group())
+print(m.groups())
+print(m.group('DISH'))
+print(m.group('FISH'))
