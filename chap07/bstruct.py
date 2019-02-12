2c4be3dae04425aa5df4651a0899e8b2ff22d8c1
chicu <chc1129@gmail.com>
2019-02-12 23:26:07 +0900

    7.2.2-7.2.4

---
+++ chap07/bstruct.py
@@ -0,0 +1,17 @@
+import struct
+valid_png_header = b'\x89PNG\r\n\x1a\n'
+data = b'\x89PNG\rn\x1a\n\x00\x00\rIHDR' + \
+    b'\x00\x00\x00\x9a\x00\x00\x00\x8d\x08\x02\x00\x00\x00\xc0'
+if data[:8] == valid_png_header:
+    width, height = struct.unpack('>LL', data[16:24])
+    print('Valid PNG, width', width, 'height', height)
+else:
+    print('Not a valid PNG')
+
+print(data[16:20])
+print(data[20:24])
+
+print(struct.pack('>L', 154))
+print(struct.pack('>L', 141))
+#print(struct.unpack('>2L', data[16:24]))
+#print(struct.unpack('>16x2L6x', data))
---
+++ chap07/btool.py
@@ -0,0 +1,12 @@
+from construct import Struct, Magic, UBInt32, Const, String
+fmt = Struct('png',
+        Magic(b'\x89PNG\r\n\x1a\n'),
+        UBInt32('length'),
+        Const(String('type', 4), b'IHDR'),
+        UBInt32('width'),
+        UBInt32('height')
+        )
+data = b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR' + \
+    b'\x00\x00\x00\x9a\x00\x00\x00\x8d\x08\x02\x00\x00\x00\xc0'
+result = fmt.parse(data)
+print(result)
