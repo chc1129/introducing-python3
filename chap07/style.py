3e338579a3773a20cc020400524571f58c349bd5
chicu <chc1129@gmail.com>
2019-02-13 23:34:22 +0900

    7.3 7-1,2,3,4,5

---
+++ chap07/style.py
@@ -0,0 +1,8 @@
+poem = '''
+    My kitty cat likes %s,
+    My kitty cat likes %s,
+    My kitty cat fell on his %s
+    And now thinks he's a %s.
+    '''
+args = ('roast beef', 'ham', 'head', 'clam')
+print(poem % args)
