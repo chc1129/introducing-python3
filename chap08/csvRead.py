948eb60d1a4b3d7eac0636218083f2ca6f6c02cf
chicu <chc1129@gmail.com>
2019-02-18 22:30:45 +0900

    update

--- chap08/csvRead.py
+++ chap08/csvRead.py
@@ -1,6 +1,6 @@
 import csv
-with open('villains', 'rt') as fin:
+with open('villains', 'rt') as fin: # コンテキストマネージャ
     cin = csv.reader(fin)
-    villains = [row for row in cin]
+    villains = [row for row in cin] # リスト内包表記を使っている
 
 print(villains)
