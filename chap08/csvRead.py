85e4c17073c7fddeeb7a03f4aa68c60af8f5cca0
chicu <chc1129@gmail.com>
2019-02-18 22:34:42 +0900

    update dictReader

--- chap08/csvRead.py
+++ chap08/csvRead.py
@@ -1,6 +1,6 @@
 import csv
 with open('villains', 'rt') as fin: # コンテキストマネージャ
-    cin = csv.reader(fin)
+    cin = csv.DictReader(fin, fieldnames=['first', 'last'])
     villains = [row for row in cin] # リスト内包表記を使っている
 
 print(villains)
