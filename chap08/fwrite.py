296e1eb39e0edc4d0e8e4a31fa083870d8dadaff
chicu <chc1129@gmail.com>
2019-02-22 03:27:53 +0900

    8-2 commit

--- chap08/fwrite.py
+++ chap08/fwrite.py
@@ -1,6 +1,12 @@
 test1 = 'This is a test of the mergency text system'
-len(test1)
+print(len(test1))
 
-outfile = open('test.text', 'wt')
+outfile = open('test.txt', 'wt')
 outfile.write(test1)
 outfile.close()
+
+with open('test.txt', 'rt') as infile:
+    test2 = infile.read()
+
+print(len(test2))
+print(test1 == test2)
