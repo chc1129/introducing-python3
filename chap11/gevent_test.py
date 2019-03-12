d80a00e844d108b53732f70fcdb7b3ee1beaebd3
chicu <chc1129@gmail.com>
2019-03-13 06:12:02 +0900

    update

--- chap11/gevent_test.py
+++ chap11/gevent_test.py
@@ -1,6 +1,7 @@
 import gevent
 from gevent import socket
-hosts = ['www.crappytaxidermy.com', 'www.walterpottertaxidermy,com', 'www.antique-taxidermy.com']
+#hosts = ['www.crappytaxidermy.com', 'www.walterpottertaxidermy,com', 'www.antique-taxidermy.com']
+hosts = ['www.crappytaxidermy.com', 'www.walterpottertaxidermy.com', 'www.taxidermy.net']
 jobs = [gevent.spawn(gevent.socket.gethostbyname, host) for host in hosts]
 gevent.joinall(jobs, timeout=5)
 for job in jobs:
