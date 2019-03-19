1de118ffbecbbbedf702190429ed54c59dfea913
chicu <chc1129@gmail.com>
2019-03-20 04:53:57 +0900

    ./cap.py

--- chap12/test_cap.py
+++ chap12/test_cap.py
@@ -24,5 +24,10 @@ class TestCap(unittest.TestCase):
         result = cap.just_do_it(text)
         self.assertEqual(result, "I'm Fresh Out Of Ideas")
 
+    def test_words_with_quotes(self):
+        text = "\"You're despicable,\" daid Daffy Duck"
+        result = cap.just_do_it(text)
+        self.assertEqual(result, "\"You're Despicable,\" Said Daffy Duck")
+
 if __name__ == '__main__':
     unittest.main()
